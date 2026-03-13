ac6e6dd0d712c6763731bc5c8f0c4b36fd3285f2
Koss <danknobs@gmail.com>
2026-03-13 14:23:30 +0800

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@swarm-cdp/core", "@swarm-cdp/simulator-data"],
+  /**
+   * Dev-only, opt-in: set CDP_DEV_POLL=1 before next dev to force webpack
+   * polling + ignored dirs (helps EMFILE when native watchers exhaust fds).
+   */
+  webpack: (config, { dev }) => {
+    if (dev && process.env.CDP_DEV_POLL === "1") {
+      config.watchOptions = {
+        ...config.watchOptions,
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: [
+          "**/node_modules/**",
+          "**/.git/**",
+          "**/.next/**",
+        ],
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
